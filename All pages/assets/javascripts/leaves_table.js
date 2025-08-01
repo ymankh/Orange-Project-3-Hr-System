3a4b9b0681adb7ba2cbb7824da9f8df9f06153a9
ymankh <ymankh1997@gmail.com>
2025-08-01 21:34:40 +0300

    feat: Refactor leaves table data in dashboard

--- All pages/assets/javascripts/leaves_table.js
+++ All pages/assets/javascripts/leaves_table.js
@@ -2,36 +2,50 @@ import { createTable, filterTable } from "./table_maker.js";
 
 let table = document.querySelector("#leaves-table");
 let filter;
-let data;
+let leavesWithEmployeesData = [];
+const url = "assets/data/employees_data.json";
+let leaves = (localStorage.leaves && JSON.parse(localStorage.leaves)) || [];
+let employeeData;
 
-// Default table heading, you can change them if you please.
-let table_heading = [
-  ["Leave ID", "leave_id"],
-  ["Employee ID", "employee_id"],
-  ["Name", "name"],
-  ["Leave Type", "type"],
-  ["Start Date", "start_date"],
-  ["End Date", "end_date"],
+// Change the this array to change the table columns
+let tableHeadings = [
+  ["Employee", "Full Name"],
+  ["Leave Type", "leaveType"],
   ["Reason", "reason"],
-  ["Approved", "approved"],
+  ["Start Date", "startDate"],
+  ["End Date", "endDate"],
 ];
 
-// Get the data from the json file.
-async function getData() {
-  if (!data) {
-    let response = await fetch("assets/data/leaves.json");
-    let leavesData = await response.json();
-    data = leavesData.flatMap(employee => 
-      employee.leave_records.map(record => ({
-        ...record,
-        employee_id: employee.employee_id,
-        name: employee.name
-      }))
-    );
-  }
+async function getEmployeeData() {
+    if (!employeeData) {
+        const response = await fetch(url);
+        employeeData = await response.json();
+    }
 }
 
-await getData();
+async function margeEmployeeDataWithLeaves() {
+    leavesWithEmployeesData = []; // Clear the array before merging
+    if (!employeeData) {
+        await getEmployeeData();
+    }
+    for (const leave of leaves) {
+        const employee = employeeData.find(
+            (employee) => employee["Employee ID"] === leave["employeeId"]
+        );
+        // Merge the employee data with the leave application
+        leavesWithEmployeesData.push({
+            ...leave,
+            ...employee,
+        });
+    }
+}
+
+async function populateLeavesData() {
+  await margeEmployeeDataWithLeaves();
+  createTable(table, leavesWithEmployeesData, tableHeadings);
+}
+
+await populateLeavesData();
 
 // For getting the filter key word from the user and filter the table
 let filterInput = document.getElementById("filter");
@@ -43,6 +57,3 @@ try {
 } catch (error) {
   console.warn("Couldn't find the filter for the table.");
 }
-
-// Add data to the table in the start of the code
-createTable(table, data, table_heading);
